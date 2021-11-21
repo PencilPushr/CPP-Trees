3f10e3aa2c1a718df4b04639a5491d7a566d94a9
archveragejoe <ajvallee15@gmail.com>
2021-11-21 22:33:21 +0000

    Initial commit for git

---
+++ treeNode.cpp
@@ -0,0 +1,5 @@
+//
+// Created by averagejoe on 11/20/21.
+//
+
+#include "treeNode.h"
---
+++ treeNode.h
@@ -0,0 +1,14 @@
+//
+// Created by averagejoe on 11/20/21.
+//
+
+#ifndef UNTITLED_TREENODE_H
+#define UNTITLED_TREENODE_H
+
+
+class treeNode {
+
+};
+
+
+#endif //UNTITLED_TREENODE_H
