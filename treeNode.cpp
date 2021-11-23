8893147afe552029574d69453f6f53a1baa99d36
archveragejoe <ajvallee15@gmail.com>
2021-11-23 17:03:52 +0000

    Initial commit for git

--- treeNode.cpp
+++ treeNode.cpp
@@ -3,3 +3,77 @@
 //
 
 #include "treeNode.h"
+
+//null constructor
+treeNode::treeNode() {
+    //don't use, implementation is pending.
+    //this will cause the program to crash!!!
+}
+
+treeNode::treeNode(std::string k, std::string v) {
+    this->key = k;
+    this->value = v;
+    this->leftChild = nullptr;
+    this->rightChild = nullptr;
+
+}
+
+int treeNode::alphabeticValue(std::string input) {
+    int total = 0;
+    for (int i = 0; i < input.length(); i++) {
+        total = total + static_cast<int>(input[i]); //remember that string is a char array -> Java -> (input.charAt(i))
+    }
+    return total;
+}
+
+void treeNode::linkToChild(treeNode* n) {
+    int self = this->alphabeticValue(this->key);
+    int child = this->alphabeticValue(n->getKey());
+    if (child < self){
+        if (this->leftChild == nullptr){
+            this->leftChild = n;
+        }else{
+            this->leftChild->linkToChild(n);
+        }
+    }else{
+        if (this->rightChild == nullptr){
+            this->rightChild = n;
+        }else{
+            this->rightChild->linkToChild(n);
+        }
+    }
+}
+
+std::string treeNode::getValueByKey(std::string k) {
+    int self = alphabeticValue(this->key);
+    int other = alphabeticValue(k);
+    if (other == self){
+        return this->value;
+    }else {
+        if (other < self) {
+            if (this->leftChild != nullptr) {
+                return this->leftChild->getValueByKey(k);
+            } else {
+                return "Sorry, no such key exists.";
+            }
+        }else{
+            if (this->rightChild != nullptr) {
+                return this->rightChild->getValueByKey(k);
+            } else {
+                return "Sorry, no such key exists.";
+            }
+        }
+    }
+}
+
+treeNode treeNode::getLeft() {
+    return treeNode();
+}
+
+treeNode treeNode::getRight() {
+    return treeNode();
+}
+
+std::string treeNode::getKey() {
+    return this->key;
+}
--- treeNode.h
+++ treeNode.h
@@ -5,8 +5,31 @@
 #ifndef UNTITLED_TREENODE_H
 #define UNTITLED_TREENODE_H
 
+#include <iostream>
+#include <string>
+
 
 class treeNode {
+
+private:
+
+//globals
+    treeNode* leftChild; //pointers for the left child
+    treeNode* rightChild; //pointers for the right child
+    std::string key;
+    std::string value;
+
+    //This is used to make the Tree ordered, if we don't want an ordered tree, DON'T USE!!!
+    int alphabeticValue(std::string input);
+
+public:
+    treeNode();
+    treeNode(std::string k, std::string v);
+    treeNode getLeft();
+    treeNode getRight();
+    std::string getKey();
+    std::string getValueByKey(std::string k);
+    void linkToChild(treeNode* n);
 
 };
 
