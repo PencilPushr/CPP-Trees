c76705f14327f0d36d5a5b87a18430ba903ee674
archjoe <ajvallee15@gmail.com>
2021-11-25 01:59:25 +0000

    updated NTreeNode

--- NtreeNode.cpp
+++ NtreeNode.cpp
@@ -4,7 +4,16 @@
 
 #include "NtreeNode.h"
 
+
+NtreeNode::NtreeNode() {
+
+}
+
 NtreeNode::~NtreeNode() {
     for(auto i=0; reinterpret_cast<NtreeNode *>(i) < children[i] ; i++)
         delete children[i];
+}
+
+NtreeNode *NtreeNode::addChild(int data, std::string content) {
+    return nullptr;
 }
--- NtreeNode.h
+++ NtreeNode.h
@@ -5,17 +5,28 @@
 #ifndef UNTITLED_NTREENODE_H
 #define UNTITLED_NTREENODE_H
 
+#include <string>
 
 class NtreeNode {
 
 private:
     NtreeNode* parent; //children nodes can only have a SINGLE parent node
     NtreeNode** children; //parent nodes can have multiple children nodes
-    //may have
+    //std::vector<NtreeNode*> children;
+
+    int data;
+    std::string content;
+
+    int depth;
+    int numOfChildren;
 
 public:
+
+    //constructors and destructors
     NtreeNode(); //default constructor -> delete later
     ~NtreeNode(); //deletes all of the children nodes
+
+    NtreeNode* addChild(int data, std::string content);
 
 };
 
